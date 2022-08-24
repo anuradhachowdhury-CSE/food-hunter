728d5091d4fedf8ef94ad1d03f53b63b483e4786
Anuradha Chowdhury <anuradhachowdhury955@gmail.com>
2022-08-24 13:18:50 +0600

    nice nice

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -4,48 +4,8 @@ import {NavLink} from 'react-router-dom'
 
 const Navbar = () => {
   return (
-  //   <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
-  //   <div class="container-fluid">
-    
-  //     <a class="navbar-brand food" href="#">Food Hunter</a><br/>
-      
-      
-  //     <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
-  //       <span class="navbar-toggler-icon"></span>
-  //     </button>
-  //     <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-  //       <div class="offcanvas-header">
-  //         {/* <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5> */}
-  //         <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-  //       </div>
-  //       <div class="offcanvas-body">
-  //         <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-  //           <li class="nav-item">
-  //             <NavLink className="nav-link"  to="/">
-  //             Home
-  //             </NavLink>
-             
-  //           </li>
-  //           <li class="nav-item">
-  //           <NavLink className="nav-link"  to="/menu">
-  //             Menu
-  //             </NavLink>
-              
-  //           </li>
-  //           <li class="nav-item">
-  //           <NavLink className="nav-link"  to="/contact">
-  //             contact
-  //             </NavLink>
-              
-  //           </li>
-            
-  //         </ul>
-          
-  //     </div>
-  //     </div>
-  //   </div>
-  // </nav>
-  <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+  
+  <nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
   <div className="container-fluid">
     <a className="heading navbar-brand" href="/">Food Hunter</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
